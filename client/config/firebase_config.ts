8f6e94406b3443e71b4b00c57c4253ddc44ed208
Andrew Arochukwu <andrewarochukwu@gmail.com>
2024-05-06 23:03:51 -0400

    Update firebase_config.ts

--- client/config/firebase_config.ts
+++ client/config/firebase_config.ts
@@ -8,13 +8,6 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCiXpK0UMunTJiM8ZvaPutS-l9yOgieWiQ",
-  authDomain: "foodframe-422304.firebaseapp.com",
-  projectId: "foodframe-422304",
-  storageBucket: "foodframe-422304.appspot.com",
-  messagingSenderId: "121525382197",
-  appId: "1:121525382197:web:f5a5a391a9a9236668957c",
-  measurementId: "G-FHLHG5N0HW",
 };
 
 // Initialize Firebase
